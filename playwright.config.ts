7bc63063661e732954d38570cfb5438fd02802da
Lean Pilar <leanpilar@gmail.com>
2026-06-20 21:32:27 +0200

    fix(test ci) svelte playwrite config for CI pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from '@playwright/test';
 
+const isDevMode = process.env.PLAYWRIGHT_DEV === 'true';
+const currentPort = isDevMode ? 5173 : 4173;
+
 export default defineConfig({
 	webServer: {
 		command: 'npm run build && npm run preview',
 		reuseExistingServer: !process.env.CI,
-		port: 5173
+		port: currentPort
 	},
 	testDir: './tests/e2e',
 	testMatch: '**/*.e2e.ts',
 	use: {
-		baseURL: 'http://localhost:5173',
+		baseURL: `http://localhost:${currentPort}`,
 		trace: 'on-first-retry'
 	}
 });
